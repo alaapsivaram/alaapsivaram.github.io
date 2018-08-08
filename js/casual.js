e74e226f68184f52d60ca77150c0f37df2766c21
alaapsivaram <alaap.sivaram@gmail.com>
2018-08-08 01:17:49 -0700

    Stable casual page with typed in jumbo text

--- js/casual.js
+++ js/casual.js
@@ -1,10 +1,18 @@
-import Typed from 'typed.js';
+
 
 var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 40
+  strings: ["Problem Solver", "Soccer Enthusiast", "Chess Player", "Sports Fan"],
+  typeSpeed: 30
 }
 
-var typed = new Typed(".element", options);
+// var typed = new Typed("#typed_test", options);
+var typed = new Typed("#typed_test", {
+	strings: ["Problem Solver", "Soccer Enthusiast", "Chess Player", "Sports Fan"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 750,
+    startDelay: 750,
+    loop: true
+  });
 console.log("TYPED:");
 console.log(typed);
