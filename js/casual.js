8b3baeb3adf3bfb56e2ccc83cc556d94eef7b1ea
alaapsivaram <alaap.sivaram@gmail.com>
2018-08-09 00:25:11 -0700

    smart backspace feature added for typed.js

--- js/casual.js
+++ js/casual.js
@@ -7,11 +7,12 @@ var options = {
 
 // var typed = new Typed("#typed_test", options);
 var typed = new Typed("#typed_test", {
-	strings: ["Problem Solver", "Soccer Enthusiast", "Chess Player", "Sports Fan", "avid reader", "poet"],
+	strings: ["I'm a Problem Solver", "I'm a Soccer Enthusiast", "I'm a Chess Player", "I'm a Sports Fan", "I'm an avid reader", "I'm a poet"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 750,
     startDelay: 750,
+    smartBackspace: true,
     loop: true
   });
 console.log("TYPED:");
