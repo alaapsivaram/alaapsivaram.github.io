f0ed6ee0f7a2958e630252adbfa45c10bec9e127
alaapsivaram <alaap.sivaram@gmail.com>
2018-08-09 23:25:17 -0700

    album updated

--- js/casual.js
+++ js/casual.js
@@ -8,8 +8,8 @@ var options = {
 // var typed = new Typed("#typed_test", options);
 var typed = new Typed("#typed_test", {
 	strings: ["I'm a Problem Solver", "I'm a Soccer Enthusiast", "I'm a Chess Player", "I'm a Sports Fan", "I'm an avid reader", "I'm a poet"],
-    typeSpeed: 50,
-    backSpeed: 50,
+    typeSpeed: 40,
+    backSpeed: 40,
     backDelay: 750,
     startDelay: 750,
     smartBackspace: true,
