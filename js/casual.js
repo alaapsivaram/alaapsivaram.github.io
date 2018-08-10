0395af83f8ef59f81c36cd241a1aee54afe8797c
Alaap Sivaram <alaap.sivaram@gmail.com>
2018-08-09 17:19:53 -0700

    Typed poem

--- js/casual.js
+++ js/casual.js
@@ -15,5 +15,13 @@ var typed = new Typed("#typed_test", {
     smartBackspace: true,
     loop: true
   });
+
+var typed1 = new Typed("#poem", {
+	strings: ["I love the world of conundrums, <br/> And shun all things humdrum; <br/> Conundrums in any form, such as,<br/>Puzzles, problems, riddles or enigmas;<br/>My favorite author is Agatha Christie,<br/>You guessed it right, I do love mystery;<br/>I am of humor a storehouse,<br/>Which explains my love for P.G. Wodehouse;<br/>The game of chess I love to play,<br/>Strategize to checkmate and to slay;<br/>By now, I’m sure you can see,<br/>Another one of my hobbies is to write poetry,<br/>And with the means of a poem, and this might sound bizarre,<br/>I’d like you meet my 'casual avatar'."],
+    typeSpeed: 25,
+    startDelay: 750,
+    loop: false
+  });
+
 console.log("TYPED:");
 console.log(typed);
